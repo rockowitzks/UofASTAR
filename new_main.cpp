a002bddf7e4023e86f77d35b7b3ebf2e4242a279
rockowitzks <37158870+rockowitzks@users.noreply.github.com>
2020-04-02 21:42:14 -0700

    tonight's wonderful changes

--- new_main.cpp
+++ new_main.cpp
@@ -29,7 +29,6 @@ class Matrix {
     Matrix(){
         // defaultly 0
         this->cost = 0;
-
         // using -1 as the initial tile value
         for(unsigned int i = 0; i < 9; i++){
             this->tiles[i] = -1;
@@ -86,8 +85,7 @@ class Matrix {
 
 // the misplaced tiles heuristic function
 int misplacedTiles(Matrix *current, int goal[]){
-    //heuristic is the number of the misplaced tiles in the state.
-    // int goalz[9] = {8,0,1,2,3,4,5,6,7};
+    // count number of misplaced tiles
     int misplaced = 0;
     // if tiles don't match, increment misplaced
     for(unsigned int i = 0; i < 9; i++){
@@ -98,7 +96,7 @@ int misplacedTiles(Matrix *current, int goal[]){
     return misplaced;
 }
 
-// the misplaced tiles heuristic function
+// the manhattan distance heuristic function
 int manhattanDistance(Matrix *current, int goal[]){
     //heuristic is the number of the misplaced tiles in the state.
     // int goalz[9] = {8,0,1,2,3,4,5,6,7};
@@ -122,9 +120,7 @@ int manhattanDistance(Matrix *current, int goal[]){
 }
 // determine if the goal has been reached
 int reachedGoal(Matrix *current, int goal[]){
-    // int goalz[9] = {8,0,1,2,3,4,5,6,7};
-
-    // if even one tile doesn't match, return 0
+  // if even one tile doesn't match, return 0
     for(unsigned int i = 0; i < 9; i++){
         if(current->tiles[i] != goal[i]){
             return 0;
@@ -143,31 +139,26 @@ Matrix* update(Matrix *current, int move){
     // grab the blank tile
     int blank = current->tiles[0];
 
-
-    // using linear indexing, m
+    // using linear indexing, encode each move
     switch(move){
         // move up
-        case 1:
+        case 0:
             tile = blank - 3;
-
             break;
             
         // move left
-        case 2:
+        case 1:
             tile = blank - 1;
-
             break;
             
         // move down
-        case 4:
+        case 2:
             tile = blank + 3;
-
             break;
 
-        // move right
+        // move right (case 3)
         default:
             tile = blank + 1;
-
             break;
     }
 
@@ -191,39 +182,42 @@ Matrix* update(Matrix *current, int move){
 // ******* A* algorithm set **********
 
 // expanding the puzzle matrix graph
+// open
 Matrix *expanded = new Matrix[1000000];
-long int root = -1;
+long int root_expanded = -1;
 
 int explored[1000000][9];
-long int root_exp = -1;
+long int root_explored = -1;
 
-void addExpanded(Matrix *a_node, int goal[]){
+void addExpanded(Matrix *a_node, int goal[], int (*h_function)(Matrix *current, int goal[])){
+
     long int i = 0;
     long int j = 0;
 
-    
     // if this is the first expansion, set the initial node
-    if(root == -1){
+    if(root_expanded == -1){
         expanded[0] = *a_node;
     }
-    // otherwise increment i for distance away from the root as long as its a better path
+    // otherwise increment i for distance away from the root_expanded as long as its a better path
     else{
-        while(i <= root && expanded[i].cost + misplacedTiles(&expanded[i], goal) <= a_node->cost + misplacedTiles(a_node, goal)){
+        while(i <= root_expanded && expanded[i].cost + h_function(&expanded[i], goal) <= a_node->cost + h_function(a_node, goal)){
             i++;
         }
-        for(j = root; j >= i; --j){
+
+        //Build expanded list (QUEUE)
+        for(j = root_expanded; j >= i; --j){
             expanded[j + 1] = expanded[j];
         }
         expanded[i] = *a_node;
     }
-    root++;
+    root_expanded++;
 }
 
 Matrix *subtractExpanded(){
     // create new subtracted Matrix
     Matrix *subtracted = new Matrix;
-    // if the root is -1, found top, return NULL
-    if(root == -1){
+    // if the root_explored is -1, found top, return NULL
+    if(root_expanded == -1){
         return NULL;
     }
     
@@ -233,24 +227,25 @@ Matrix *subtractExpanded(){
         }
         subtracted->parent = expanded[0].parent;
         subtracted->cost = expanded[0].cost;
-        for(unsigned long int i = 0; i < root; i++){
+        for(unsigned long int i = 0; i < root_expanded; i++){
             expanded[i] = expanded[i+1];
         }
-        root--;
+        root_expanded--;
         return subtracted;
     }
 }
 
+// increment the counter for explored, add the matrix tile by tile to the pile
 void addExplored(int *tile){
-    root_exp++;
+    root_explored++;
     for(int i = 0; i < 9; i++){
-        explored[root_exp][i] = tile[i];
+        explored[root_explored][i] = tile[i];
     }
 }
 
 int alreadyExplored(int *tile){
     int count = 0;
-    for(unsigned long int i = 0; i <= root_exp; i++){
+    for(unsigned long int i = 0; i <= root_explored; i++){
         count = 0;
         for(int j = 0; j < 9; j++){
             if(explored[i][j] == tile[j]){
@@ -278,7 +273,7 @@ void getMove(int array[]){
   }
   */
 }
-void astar(Matrix initial_state, int goal[]){
+void astar(Matrix initial_state, int goal[], int (*h_function)(Matrix *current, int goal[])){
     // create a new matrix pointer
     Matrix *initial = new Matrix;
     // point to the initial node
@@ -289,17 +284,24 @@ void astar(Matrix initial_state, int goal[]){
     
     int moves;
     int moves_avail[4];
+    int numExploredMatrices = 0;
 
-    addExpanded(initial, goal);
+
+
+    // expand the initial node
+    addExpanded(initial, goal, h_function);
 
     while(1){
         // reached the end without finding the goal state
-        if(root == -1){
+        if(root_expanded == -1){
             cout<<"no solution found"<<endl;
             return;
         }
+        // prune the branch
         temp_matrix = subtractExpanded();
+        // add those tiles from pruned branch to explored.
         addExplored(temp_matrix->tiles);
+
         if(reachedGoal(temp_matrix, goal)){
             cout<<"solution found"<<endl;
             while(temp_matrix){
@@ -316,33 +318,33 @@ void astar(Matrix initial_state, int goal[]){
                 cout << endl << endl;
                 temp_matrix = temp_matrix->parent;
             }
+            //Prints number of explored Matrices
+            cout << "Number of matrices visited: " << numExploredMatrices << endl;
             return;
         }
 
+        for(unsigned int i = 0; i < 4; i++){
+            moves_avail[i] = 0;
+        }
+        
         moves = temp_matrix->moves();
-        switch(moves)
-        {
+        switch(moves){
             case 12:  // RD
-                moves_avail[0]=0;
-                moves_avail[1]=0;
                 moves_avail[2]=1;
                 moves_avail[3]=1;
                 break;
             case 14: // LRD
-                moves_avail[0]=0;
                 moves_avail[1]=1;
                 moves_avail[2]=1;
                 moves_avail[3]=1;
                 break;
             case 6: // LD
-                moves_avail[0]=0;
                 moves_avail[1]=1;
                 moves_avail[2]=1;
                 moves_avail[3]=0;
                 break;
             case 13: // URD
                 moves_avail[0]=1;
-                moves_avail[1]=0;
                 moves_avail[2]=1;
                 moves_avail[3]=1;
                 break;
@@ -355,43 +357,35 @@ void astar(Matrix initial_state, int goal[]){
             case 7: // ULD
                 moves_avail[0]=1;
                 moves_avail[1]=1;
-                moves_avail[2]=0;
-                moves_avail[3]=0;
                 break;
             case 9: // UR
                 moves_avail[0]=1;
-                moves_avail[1]=0;
-                moves_avail[2]=0;
                 moves_avail[3]=1;
                 break;
             case 11: // LUR
                 moves_avail[0]=1;
                 moves_avail[1]=1;
-                moves_avail[2]=0;
                 moves_avail[3]=1;
                 break;
             case 3: // LU
                 moves_avail[0]=1;
                 moves_avail[1]=1;
-                moves_avail[2]=0;
-                moves_avail[3]=0;
                 break;
         }
         
-        for(int i = 0; i < 4; i++)
-        {
+        for(int i = 0; i < 4; i++){
             // for every available move, update the new matrix
             if(moves_avail[i]){
                 // update the new matrix
-                new_matrix = update(temp_matrix,pow(2,i));
+                new_matrix = update(temp_matrix,i); // 0, 1, 2 3
                 // if the new matrix is not part of the explored set, expand and explore it
-                if(alreadyExplored(new_matrix->tiles) == 0)
-                    addExpanded(new_matrix, goal);
+                if(alreadyExplored(new_matrix->tiles) == 0) {
+                    addExpanded(new_matrix, goal, h_function);
+                    numExploredMatrices++;
+                }
             }
         }
-
     }
-    
 }
 
 
@@ -402,23 +396,42 @@ int main(int argc, const char * argv[]) {
     
 
     cout<<"8 Puzzle Solver"<<endl;
-    cout<<"Please Enter the Initial State"<<endl;
 
-    for(unsigned int i = 0; i < 9; i++){
-        cin>>value;
-//        initial_state.tiles[i] = value;
-        initial_state.tiles[value] = i;
-    }
+    // cout<<"Please Enter the Initial State"<<endl;
+    // for(unsigned int i = 0; i < 9; i++){
+    //     cin >> value;
+    //     initial_state.tiles[value] = i;
+    // }
+
+    // DEBUG INITIAL State
+    initial_state.tiles[0] = 8;
+    initial_state.tiles[1] = 4;
+    initial_state.tiles[2] = 6;
+    initial_state.tiles[3] = 1;
+    initial_state.tiles[4] = 3;
+    initial_state.tiles[5] = 2;
+    initial_state.tiles[6] = 5;
+    initial_state.tiles[7] = 7;
+    initial_state.tiles[8] = 0;
+
     
     value = 0;
-    int goal[9];
+    int goal[9] = {4,0,1,2,5,8,7,6,3};
+                //{ 0,1,2,3,5,4,6,7,8 }
+
+    //0 1 2
+    //3 5 4
+    //6 7 8
+    //int goal[9];
     
-    cout<<"Please Enter the Goal State"<<endl;
-    for(unsigned int i = 0; i < 9; i++){
-            cin >> value;
-    //        initial_state.tiles[i] = value;
-            goal[value] = i;
-        }
+//     cout<<"Please Enter the Goal State"<<endl;
+//     for(unsigned int i = 0; i < 9; i++){
+//         cin >> value;
+// //        initial_state.tiles[i] = value;
+//         goal[value] = i;
+//     }
+
+   
     
     //cout << "You entered ";
     //for(unsigned int i = 0; i < 9; i++){
@@ -427,18 +440,19 @@ int main(int argc, const char * argv[]) {
     //cout << endl;
     
     //Determine heuristic
-    function<void()> h_function;
+    int (*h_function)(Matrix *current, int goal[]);
+    //function<void()> h_function;
     int h_funct_val = 0;
     
-    cout << "Please Enter 0 for misplaced tiles or 1 for Manhattan" << endl;
+    cout << "Please Enter 0 for Misplaced Tiles or 1 for Manhattan" << endl;
     cin >> h_funct_val;
     
     if (h_funct_val == 1){
-      h_function = &misplacedTiles;
+      h_function = &manhattanDistance;
       cout << "You chose Manhattan Heuristic" << endl;
     }
     else {
-      h_function = &manhattanDistance;
+      h_function = &misplacedTiles;
       cout << "You chose Misplaced Tiles heuristic" << endl;
     }
 
@@ -448,7 +462,7 @@ int main(int argc, const char * argv[]) {
     // TODO: allow user entry of the goal state
     cout<<"Processing..." << endl;
 
-    astar(initial_state, goal);
+    astar(initial_state, goal, h_function);
 
     
     
